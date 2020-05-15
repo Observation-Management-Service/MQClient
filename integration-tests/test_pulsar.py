01a37c5a5cacbb8ea12aef0f1e54ad9c5b464f31
Ric Evans <emejqz@gmail.com>
2020-05-15 16:36:18 -0500

    remove prints

--- integration-tests/test_pulsar.py
+++ integration-tests/test_pulsar.py
@@ -13,14 +13,11 @@ def test_queue():
     q.send(data)
 
     with q.recv_one() as d:
-        print(f"OUT!! {d}")
         assert d == data
 
     data = [1, '2', data]
     for d in data:
-        print(f"SEND:: {d}")
         q.send(d)
 
     for i, d in enumerate(q.recv(timeout=1)):
-        print(f"RECV:: {i}:{d}")
         assert d == data[i]
