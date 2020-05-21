d60650184c201a884b8e7a4adccdb1c68e0b42da
Ric Evans <emejqz@gmail.com>
2020-05-21 12:39:52 -0500

    refactor rabbitmq integration test class name, so it runs

--- integration_tests/test_rabbitmq.py
+++ integration_tests/test_rabbitmq.py
@@ -8,7 +8,7 @@ from MQClient.backends import rabbitmq
 from .common_tests import PubSub, queue_name  # noqa: F401 # pylint: disable=W0611
 
 
-class PulsarTests(PubSub):
+class TestRabbitMQ(PubSub):
     """Run PubSub integration tests with RabbitMQ backend."""
 
     backend = rabbitmq
