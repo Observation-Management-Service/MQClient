f6bedd98b0b830969a4dfbaf3aee480b6ea3c2c4
Ric Evans <emejqz@gmail.com>
2020-06-23 16:25:36 -0500

    add logging to back-ends

---
+++ MQClient/backends/logging_strings.py
@@ -0,0 +1,36 @@
+"""House common logging strings."""
+
+SENDING_MESSAGE = "[send_message()] Sending message..."
+SENT_MESSAGE = "[send_message()] Sent message."
+
+GETMSG_RECEIVE_MESSAGE = "[get_message()] Trying to receive message..."
+GETMSG_RECEIVED_MESSAGE = "[get_message()] Received message."
+GETMSG_NO_MESSAGE = "[get_message()] Didn't receive message. Returning None."
+GETMSG_TIMEOUT_ERROR = "[get_message()] Timeout error. Returning None."
+GETMSG_CONNECTION_ERROR_TRY_AGAIN = "[get_message()] Connection error. Trying again."
+GETMSG_RAISE_OTHER_ERROR = "[get_message()] Other error. Raising Exception."
+GETMSG_CONNECTION_ERROR_MAX_RETRIES = "[get_message()] Connection error. Reached max retries. Raising Exception."
+
+ACKING_MESSAGE = "[ack_message()] Acking message..."
+ACKD_MESSAGE = "[ack_message()] Ack'd message."
+
+NACKING_MESSAGE = "[reject_message()] Nacking message..."
+NACKD_MESSAGE = "[reject_message()] Nack'd message."
+
+MSGGEN_GET_NEW_MESSAGE = "[message_generator()] Getting a new message..."
+MSGGEN_NO_MESSAGE_LOOK_BACK_IN_QUEUE = "[message_generator()] No messages in idle timeout window."
+MSGGEN_PROPAGATING_ERROR = "[message_generator()] Propagating error."
+MSGGEN_ERROR_DOWNSTREAM = "[message_generator()] Error downstream:"
+MSGGEN_CLOSED_QUEUE = "[message_generator()] Closed queue."
+
+TRYCALL_CONNECTION_CLOSED_BY_BROKER = "[try_call()] ConnectionClosedByBroker..."
+TRYCALL_AMQP_CONNECTION_ERROR = "[try_call()] AMQPConnectionError..."
+TRYCALL_RAISE_AMQP_CHANNEL_ERROR = "[try_call()] AMQPChannelError. Raising Exception."
+TRYCALL_CONNECTION_ERROR_TRY_AGAIN = "[try_call()] Connection error. Trying again."
+TRYCALL_CONNECTION_ERROR_MAX_RETRIES = "[try_call()] Connection error. Reached max retries. Raising Exception."
+
+TRYYIELD_CONNECTION_CLOSED_BY_BROKER = "[try_yield()] ConnectionClosedByBroker..."
+TRYYIELD_AMQP_CONNECTION_ERROR = "[try_yield()] AMQPConnectionError..."
+TRYYIELD_RAISE_AMQP_CHANNEL_ERROR = "[try_yield()] AMQPChannelError. Raising Exception."
+TRYYIELD_CONNECTION_ERROR_TRY_AGAIN = "[try_yield()] Connection error. Trying again."
+TRYYIELD_CONNECTION_ERROR_MAX_RETRIES = "[try_yield()] Connection error. Reached max retries. Raising Exception."
---
+++ tests/backends/test_apachepulsar.py
@@ -0,0 +1,128 @@
+"""Unit Tests for Pulsar Backend."""
+
+# pylint: disable=redefined-outer-name
+
+import logging
+from typing import Any
+
+import pytest  # type: ignore
+
+# local imports
+from MQClient.backends import apachepulsar
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+
+@pytest.fixture  # type: ignore
+def mock_ap(mocker: Any) -> Any:
+    """Patch mock_ap."""
+    return mocker.patch('pulsar.Client')
+
+
+def test_create_pub_queue(mock_ap: Any) -> None:
+    """Test creating pub queue."""
+    q = apachepulsar.create_pub_queue("localhost", "test")
+    assert q.topic == "test"
+    mock_ap.return_value.create_producer.assert_called()
+
+
+def test_create_sub_queue(mock_ap: Any) -> None:
+    """Test creating sub queue."""
+    q = apachepulsar.create_sub_queue("localhost", "test", prefetch=213)
+    assert q.topic == "test"
+    assert q.prefetch == 213
+    mock_ap.return_value.subscribe.assert_called()
+
+
+def test_send_message(mock_ap: Any) -> None:
+    """Test sending message."""
+    q = apachepulsar.create_pub_queue("localhost", "test")
+    apachepulsar.send_message(q, b"foo, bar, baz")
+    mock_ap.return_value.create_producer.return_value.send.assert_called_with(b'foo, bar, baz')
+
+
+def test_get_message(mock_ap: Any) -> None:
+    """Test getting message."""
+    q = apachepulsar.create_sub_queue("localhost", "test")
+    mock_ap.return_value.subscribe.return_value.receive.return_value.data.return_value = b'foo, bar'
+    mock_ap.return_value.subscribe.return_value.receive.return_value.message_id.return_value = 12
+    m = apachepulsar.get_message(q)
+    assert m is not None
+    assert m.msg_id == 12
+    assert m.data == b'foo, bar'
+
+
+def test_ack_message(mock_ap: Any) -> None:
+    """Test acking message."""
+    q = apachepulsar.create_sub_queue("localhost", "test")
+    apachepulsar.ack_message(q, 12)
+    mock_ap.return_value.subscribe.return_value.acknowledge.assert_called_with(12)
+
+
+def test_reject_message(mock_ap: Any) -> None:
+    """Test rejecting message."""
+    q = apachepulsar.create_sub_queue("localhost", "test")
+    apachepulsar.reject_message(q, 12)
+    mock_ap.return_value.subscribe.return_value.negative_acknowledge.assert_called_with(12)
+
+
+def test_message_generator(mock_ap: Any) -> None:
+    """Test message generator."""
+    q = apachepulsar.create_sub_queue("localhost", "test")
+    mock_ap.return_value.subscribe.return_value.receive.return_value.data.side_effect = [
+        b'foo, bar', b'baz']
+    mock_ap.return_value.subscribe.return_value.receive.return_value.message_id.side_effect = [
+        12, 20]
+    m = None
+    for i, x in enumerate(apachepulsar.message_generator(q)):
+        if i > 0:
+            break
+        m = x
+    assert m is not None
+    assert m.msg_id == 12
+    assert m.data == b'foo, bar'
+    mock_ap.return_value.subscribe.return_value.acknowledge.assert_called_with(12)
+    mock_ap.return_value.close.assert_called()
+
+
+def test_message_generator_2(mock_ap: Any) -> None:
+    """Test message generator."""
+    q = apachepulsar.create_sub_queue("localhost", "test")
+    mock_ap.return_value.subscribe.return_value.receive.return_value.data.side_effect = [
+        b'foo, bar', None]
+    mock_ap.return_value.subscribe.return_value.receive.return_value.message_id.side_effect = [
+        12, None]
+    m = None
+    for i, x in enumerate(apachepulsar.message_generator(q)):
+        assert i < 1
+        m = x
+    assert m is not None
+    assert m.msg_id == 12
+    assert m.data == b'foo, bar'
+    mock_ap.return_value.subscribe.return_value.acknowledge.assert_called_with(12)
+    mock_ap.return_value.close.assert_called()
+
+
+def test_message_generator_nack(mock_ap: Any) -> None:
+    """Failure-test message generator.
+
+    Generator should send nack.
+    """
+    q = apachepulsar.create_sub_queue("localhost", "test")
+    mock_ap.return_value.subscribe.return_value.receive.return_value.data.side_effect = [
+        b'foo, bar', None]
+    mock_ap.return_value.subscribe.return_value.receive.return_value.message_id.side_effect = [
+        12, None]
+    m = None
+    for i, x in enumerate(apachepulsar.message_generator(q)):
+        assert i < 1
+        m = x
+
+    mock_ap.return_value.subscribe.return_value.receive.side_effect = Exception()
+    with pytest.raises(Exception):
+        _ = list(apachepulsar.message_generator(q, propagate_error=True))
+
+    assert 0 == 5
+
+    mock_ap.return_value.subscribe.return_value.receive.side_effect = Exception()
+    _ = list(apachepulsar.message_generator(q, propagate_error=False))
