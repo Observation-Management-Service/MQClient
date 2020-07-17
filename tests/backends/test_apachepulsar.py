34629c75a7f65c363f71fe1f325ae7e2d3dc35be
Ric Evans <emejqz@gmail.com>
2020-07-17 17:46:24 -0500

    use unique queue names in unit testing

--- tests/backends/test_apachepulsar.py
+++ tests/backends/test_apachepulsar.py
@@ -3,6 +3,7 @@
 # pylint: disable=redefined-outer-name
 
 import logging
+import uuid
 from typing import Any, List, Optional
 
 import pytest  # type: ignore
@@ -19,31 +20,39 @@ def mock_ap(mocker: Any) -> Any:
     return mocker.patch('pulsar.Client')
 
 
-def test_create_pub_queue(mock_ap: Any) -> None:
+@pytest.fixture  # type: ignore
+def queue_name() -> str:
+    """Get random queue name."""
+    name = uuid.uuid4().hex
+    print(f"NAME :: {name}")
+    return name
+
+
+def test_create_pub_queue(mock_ap: Any, queue_name: str) -> None:
     """Test creating pub queue."""
-    q = apachepulsar.Backend().create_pub_queue("localhost", "test")
-    assert q.topic == "test"
+    q = apachepulsar.Backend().create_pub_queue("localhost", queue_name)
+    assert q.topic == queue_name
     mock_ap.return_value.create_producer.assert_called()
 
 
-def test_create_sub_queue(mock_ap: Any) -> None:
+def test_create_sub_queue(mock_ap: Any, queue_name: str) -> None:
     """Test creating sub queue."""
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test", prefetch=213)
-    assert q.topic == "test"
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name, prefetch=213)
+    assert q.topic == queue_name
     assert q.prefetch == 213
     mock_ap.return_value.subscribe.assert_called()
 
 
-def test_send_message(mock_ap: Any) -> None:
+def test_send_message(mock_ap: Any, queue_name: str) -> None:
     """Test sending message."""
-    q = apachepulsar.Backend().create_pub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_pub_queue("localhost", queue_name)
     q.send_message(b"foo, bar, baz")
     mock_ap.return_value.create_producer.return_value.send.assert_called_with(b'foo, bar, baz')
 
 
-def test_get_message(mock_ap: Any) -> None:
+def test_get_message(mock_ap: Any, queue_name: str) -> None:
     """Test getting message."""
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
     mock_ap.return_value.subscribe.return_value.receive.return_value.data.return_value = b'foo, bar'
     mock_ap.return_value.subscribe.return_value.receive.return_value.message_id.return_value = 12
     m = q.get_message()
@@ -53,23 +62,23 @@ def test_get_message(mock_ap: Any) -> None:
     assert m.data == b'foo, bar'
 
 
-def test_ack_message(mock_ap: Any) -> None:
+def test_ack_message(mock_ap: Any, queue_name: str) -> None:
     """Test acking message."""
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
     q.ack_message(12)
     mock_ap.return_value.subscribe.return_value.acknowledge.assert_called_with(12)
 
 
-def test_reject_message(mock_ap: Any) -> None:
+def test_reject_message(mock_ap: Any, queue_name: str) -> None:
     """Test rejecting message."""
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
     q.reject_message(12)
     mock_ap.return_value.subscribe.return_value.negative_acknowledge.assert_called_with(12)
 
 
-def test_message_generator_0(mock_ap: Any) -> None:
+def test_message_generator_0(mock_ap: Any, queue_name: str) -> None:
     """Test message generator."""
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
     num_msgs = 100
 
     fake_data = [f'baz-{i}'.encode('utf-8') for i in range(num_msgs)]  # type: List[Optional[bytes]]
@@ -91,9 +100,9 @@ def test_message_generator_0(mock_ap: Any) -> None:
     mock_ap.return_value.close.assert_called()
 
 
-def test_message_generator_1(mock_ap: Any) -> None:
+def test_message_generator_1(mock_ap: Any, queue_name: str) -> None:
     """Test message generator."""
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
     mock_ap.return_value.subscribe.return_value.receive.return_value.data.side_effect = [
         b'foo, bar', b'baz']
     mock_ap.return_value.subscribe.return_value.receive.return_value.message_id.side_effect = [
@@ -113,9 +122,9 @@ def test_message_generator_1(mock_ap: Any) -> None:
     mock_ap.return_value.close.assert_called()
 
 
-def test_message_generator_2(mock_ap: Any) -> None:
+def test_message_generator_2(mock_ap: Any, queue_name: str) -> None:
     """Test message generator."""
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
     mock_ap.return_value.subscribe.return_value.receive.return_value.data.side_effect = [
         b'foo, bar',
         None  # signifies end of stream -- not actually a message
@@ -138,13 +147,13 @@ def test_message_generator_2(mock_ap: Any) -> None:
     mock_ap.return_value.close.assert_called()
 
 
-def test_message_generator_upstream_error(mock_ap: Any) -> None:
+def test_message_generator_upstream_error(mock_ap: Any, queue_name: str) -> None:
     """Failure-test message generator.
 
     Generator should raise Exception originating upstream (a.k.a. from
     pulsar-package code).
     """
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
 
     mock_ap.return_value.subscribe.return_value.receive.side_effect = Exception()
     with pytest.raises(Exception):
@@ -158,12 +167,12 @@ def test_message_generator_upstream_error(mock_ap: Any) -> None:
     mock_ap.return_value.close.assert_called()
 
 
-def test_message_generator_no_auto_ack(mock_ap: Any) -> None:
+def test_message_generator_no_auto_ack(mock_ap: Any, queue_name: str) -> None:
     """Test message generator.
 
     Generator should not ack messages.
     """
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
 
     fake_data = [b'baz-0', b'baz-1', b'baz-2']  # type: List[Optional[bytes]]
     fake_data += [None]  # signifies end of stream -- not actually a message
@@ -186,13 +195,13 @@ def test_message_generator_no_auto_ack(mock_ap: Any) -> None:
         i += 1
 
 
-def test_message_generator_propagate_error(mock_ap: Any) -> None:
+def test_message_generator_propagate_error(mock_ap: Any, queue_name: str) -> None:
     """Failure-test message generator.
 
     Generator should raise Exception, nack, and close. Unlike in an
     integration test, nacked messages are not put back on the queue.
     """
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
 
     fake_data = [b'baz-0', b'baz-1', b'baz-2']
     mock_ap.return_value.subscribe.return_value.receive.return_value.data.side_effect = fake_data
@@ -220,13 +229,13 @@ def test_message_generator_propagate_error(mock_ap: Any) -> None:
         i += 1
 
 
-def test_message_generator_suppress_error(mock_ap: Any) -> None:
+def test_message_generator_suppress_error(mock_ap: Any, queue_name: str) -> None:
     """Failure-test message generator.
 
     Generator should not raise Exception. Unlike in an integration test,
     nacked messages are not put back on the queue.
     """
-    q = apachepulsar.Backend().create_sub_queue("localhost", "test")
+    q = apachepulsar.Backend().create_sub_queue("localhost", queue_name)
     num_msgs = 11
     if num_msgs % 2 == 0:
         raise RuntimeError("`num_msgs` must be odd, so last message is nacked")
--- tests/backends/test_rabbitmq.py
+++ tests/backends/test_rabbitmq.py
@@ -4,6 +4,7 @@
 
 import logging
 import unittest
+import uuid
 from typing import Any, List, Optional, Tuple
 from unittest.mock import MagicMock
 
@@ -21,35 +22,43 @@ def mock_pika(mocker: Any) -> Any:
     return mocker.patch('pika.BlockingConnection')
 
 
-def test_create_pub_queue(mock_pika: Any) -> None:
+@pytest.fixture  # type: ignore
+def queue_name() -> str:
+    """Get random queue name."""
+    name = uuid.uuid4().hex
+    print(f"NAME :: {name}")
+    return name
+
+
+def test_create_pub_queue(mock_pika: Any, queue_name: str) -> None:
     """Test creating pub queue."""
-    q = rabbitmq.Backend().create_pub_queue("localhost", "test")
-    assert q.queue == "test"
+    q = rabbitmq.Backend().create_pub_queue("localhost", queue_name)
+    assert q.queue == queue_name
     mock_pika.return_value.channel.assert_called()
 
 
-def test_create_sub_queue(mock_pika: Any) -> None:
+def test_create_sub_queue(mock_pika: Any, queue_name: str) -> None:
     """Test creating sub queue."""
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test", prefetch=213)
-    assert q.queue == "test"
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name, prefetch=213)
+    assert q.queue == queue_name
     assert q.prefetch == 213
     mock_pika.return_value.channel.assert_called()
 
 
-def test_send_message(mock_pika: Any) -> None:
+def test_send_message(mock_pika: Any, queue_name: str) -> None:
     """Test sending message."""
-    q = rabbitmq.Backend().create_pub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_pub_queue("localhost", queue_name)
     q.send_message(b"foo, bar, baz")
     mock_pika.return_value.channel.return_value.basic_publish.assert_called_with(
         exchange='',
-        routing_key='test',
+        routing_key=queue_name,
         body=b'foo, bar, baz',
     )
 
 
-def test_get_message(mock_pika: Any) -> None:
+def test_get_message(mock_pika: Any, queue_name: str) -> None:
     """Test getting message."""
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
     fake_message = (MagicMock(delivery_tag=12), None, b'foo, bar')
     mock_pika.return_value.channel.return_value.basic_get.return_value = fake_message
     m = q.get_message()
@@ -58,23 +67,23 @@ def test_get_message(mock_pika: Any) -> None:
     assert m.data == b'foo, bar'
 
 
-def test_ack_message(mock_pika: Any) -> None:
+def test_ack_message(mock_pika: Any, queue_name: str) -> None:
     """Test acking message."""
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
     q.ack_message(12)
     mock_pika.return_value.channel.return_value.basic_ack.assert_called_with(12)
 
 
-def test_reject_message(mock_pika: Any) -> None:
+def test_reject_message(mock_pika: Any, queue_name: str) -> None:
     """Test rejecting message."""
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
     q.reject_message(12)
     mock_pika.return_value.channel.return_value.basic_nack.assert_called_with(12)
 
 
-def test_message_generator_0(mock_pika: Any) -> None:
+def test_message_generator_0(mock_pika: Any, queue_name: str) -> None:
     """Test message generator."""
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
     num_msgs = 100
 
     fake_messages = [(MagicMock(delivery_tag=i * 10), None, f'baz-{i}'.encode('utf-8')) for i in range(num_msgs)]  # type: List[Tuple[Optional[MagicMock], None, Optional[bytes]]]
@@ -92,9 +101,9 @@ def test_message_generator_0(mock_pika: Any) -> None:
     mock_pika.return_value.channel.return_value.cancel.assert_called()
 
 
-def test_message_generator_1(mock_pika: Any) -> None:
+def test_message_generator_1(mock_pika: Any, queue_name: str) -> None:
     """Test message generator."""
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
     fake_message = (MagicMock(delivery_tag=12), None, b'foo, bar')
     fake_message2 = (MagicMock(delivery_tag=20), None, b'baz')
     mock_pika.return_value.channel.return_value.consume.return_value = [fake_message, fake_message2]
@@ -111,9 +120,9 @@ def test_message_generator_1(mock_pika: Any) -> None:
     mock_pika.return_value.channel.return_value.cancel.assert_called()
 
 
-def test_message_generator_2(mock_pika: Any) -> None:
+def test_message_generator_2(mock_pika: Any, queue_name: str) -> None:
     """Test message generator."""
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
     fake_message = (MagicMock(delivery_tag=12), None, b'foo, bar')
     fake_message2 = (None, None, None)  # signifies end of stream -- not actually a message
     mock_pika.return_value.channel.return_value.consume.return_value = [fake_message, fake_message2]
@@ -128,13 +137,13 @@ def test_message_generator_2(mock_pika: Any) -> None:
     mock_pika.return_value.channel.return_value.cancel.assert_called()
 
 
-def test_message_generator_upstream_error(mock_pika: Any) -> None:
+def test_message_generator_upstream_error(mock_pika: Any, queue_name: str) -> None:
     """Failure-test message generator.
 
     Generator should raise Exception originating upstream (a.k.a. from
     pika-package code).
     """
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
 
     err_msg = (unittest.mock.ANY, None, b'foo, bar')
     mock_pika.return_value.channel.return_value.consume.return_value = [err_msg]
@@ -150,12 +159,12 @@ def test_message_generator_upstream_error(mock_pika: Any) -> None:
     mock_pika.return_value.channel.return_value.cancel.assert_called()
 
 
-def test_message_generator_no_auto_ack(mock_pika: Any) -> None:
+def test_message_generator_no_auto_ack(mock_pika: Any, queue_name: str) -> None:
     """Test message generator.
 
     Generator should not ack messages.
     """
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
 
     fake_messages = [
         (MagicMock(delivery_tag=0), None, b'baz-0'),
@@ -179,13 +188,13 @@ def test_message_generator_no_auto_ack(mock_pika: Any) -> None:
         i += 1
 
 
-def test_message_generator_propagate_error(mock_pika: Any) -> None:
+def test_message_generator_propagate_error(mock_pika: Any, queue_name: str) -> None:
     """Failure-test message generator.
 
     Generator should raise Exception, nack, and close. Unlike in an
     integration test, nacked messages are not put back on the queue.
     """
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
 
     fake_messages = [
         (MagicMock(delivery_tag=0), None, b'baz-0'),
@@ -215,13 +224,13 @@ def test_message_generator_propagate_error(mock_pika: Any) -> None:
         i += 1
 
 
-def test_message_generator_suppress_error(mock_pika: Any) -> None:
+def test_message_generator_suppress_error(mock_pika: Any, queue_name: str) -> None:
     """Failure-test message generator.
 
     Generator should not raise Exception. Unlike in an integration test,
     nacked messages are not put back on the queue.
     """
-    q = rabbitmq.Backend().create_sub_queue("localhost", "test")
+    q = rabbitmq.Backend().create_sub_queue("localhost", queue_name)
     num_msgs = 11
     if num_msgs % 2 == 0:
         raise RuntimeError("`num_msgs` must be odd, so last message is nacked")
