f0966295e6d4e5c1380250832c1046d9f41c0ca5
Ric Evans <emejqz@gmail.com>
2021-06-22 18:27:39 -0500

    add pub sub examples from GCloud's docs with setup scripts

---
+++ examples/gcp/run-using-emulator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# see https://cloud.google.com/pubsub/docs/emulator#using_the_emulator
+gcloud beta emulators pubsub start --project=abc123 &
+export PUBSUB_EMULATOR_HOST=localhost:8085
+python publisher.py abc123 create top456
+python subscriber.py abc123 create top456 sub789
+python publisher.py abc123 publish top456
+python subscriber.py abc123 receive sub789
