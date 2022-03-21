f8758a9ce90bd203a4042b4137ca1919f16f6d03
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2022-03-21 18:42:26 -0500

    Updates for PyPI Publish [minor] (#40)
    Co-authored-by: github-actions <github-actions@github.com>

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-#!/usr/bin/env python
 """Setup."""
 
-import os
-import subprocess
 
-from setuptools import setup  # type: ignore[import]
+from setuptools import setup  # type: ignore[import] # mypy error in GH Action
 
-subprocess.run(
-    "pip install git+https://github.com/WIPACrepo/wipac-dev-tools.git".split(),
-    check=True,
-)
-from wipac_dev_tools import SetupShop  # noqa: E402  # pylint: disable=C0413
-
-shop = SetupShop(
-    "mqclient",
-    os.path.abspath(os.path.dirname(__file__)),
-    ((3, 6), (3, 9)),
-    "the base abstraction for the Message Queue Client",
-)
-
-setup(
-    url="https://github.com/WIPACrepo/MQClient",
-    package_data={shop.name: ["py.typed", "requirements.txt"]},
-    **shop.get_kwargs(subpackages=["abstract_backend_tests"]),
-)
+setup()
