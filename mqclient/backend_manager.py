340b00a86a4c2af7e2bde3d5f684ae0c0a745c3b
Ric Evans <emejqz@gmail.com>
2022-09-22 13:24:18 -0500

    add `ModuleNotFoundError` logic/ messaging

--- mqclient/backend_manager.py
+++ mqclient/backend_manager.py
@@ -6,30 +6,37 @@ from .backend_interface import Backend
 def get_backend(backend_name: str) -> Backend:
     """Get the `Backend` instance per the given name."""
 
-    # Pulsar
-    if backend_name.lower() == "pulsar":
-        from .backends import apachepulsar
+    try:
+        # Pulsar
+        if backend_name.lower() == "pulsar":
+            from .backends import apachepulsar
 
-        return apachepulsar.Backend
+            return apachepulsar.Backend
 
-    # GCP
-    elif backend_name.lower() == "gcp":
-        from .backends import gcp
+        # GCP
+        elif backend_name.lower() == "gcp":
+            from .backends import gcp
 
-        return gcp.Backend
+            return gcp.Backend
 
-    # NATS
-    elif backend_name.lower() == "nats":
-        from .backends import nats
+        # NATS
+        elif backend_name.lower() == "nats":
+            from .backends import nats
 
-        return nats.Backend
+            return nats.Backend
 
-    # RabbitMQ
-    elif backend_name.lower() == "rabbitmq":
-        from .backends import rabbitmq
+        # RabbitMQ
+        elif backend_name.lower() == "rabbitmq":
+            from .backends import rabbitmq
 
-        return rabbitmq.Backend
+            return rabbitmq.Backend
 
-    # Error
-    else:
-        raise RuntimeError(f"Unknown backend: {backend_name}")
+        # Error
+        else:
+            raise RuntimeError(f"Unknown backend: {backend_name}")
+
+    except ModuleNotFoundError as e:
+        raise RuntimeError(
+            f"Install 'mqclient[{backend_name.lower()}]' "
+            f"if you want to use the '{backend_name}' backend"
+        ) from e
