4a9f24750f9e98b338b08b9d69cbd31af08c7614
Ric Evans <emejqz@gmail.com>
2022-09-22 13:16:10 -0500

    clean up `get_backend()`

--- mqclient/backend_manager.py
+++ mqclient/backend_manager.py
@@ -1,13 +1,35 @@
 """Manage the different backends."""
 
-from . import backends
 from .backend_interface import Backend
 
 
 def get_backend(backend_name: str) -> Backend:
     """Get the `Backend` instance per the given name."""
-    try:
-        return getattr(backends, backend_name).Backend  # type: ignore[no-any-return]
-        # TODO - we probably need to except ImportError
-    except AttributeError as e:
-        raise RuntimeError(f"Unknown backend: {backend_name}") from e
+
+    # Pulsar
+    if backend_name.lower() == "pulsar":
+        from .backends import apachepulsar
+
+        return apachepulsar.Backend
+
+    # GCP
+    elif backend_name.lower() == "gcp":
+        from .backends import gcp
+
+        return gcp.Backend
+
+    # NATS
+    elif backend_name.lower() == "nats":
+        from .backends import nats
+
+        return nats.Backend
+
+    # RabbitMQ
+    elif backend_name.lower() == "rabbitmq":
+        from .backends import rabbitmq
+
+        return rabbitmq.Backend
+
+    # Error
+    else:
+        raise RuntimeError(f"Unknown backend: {backend_name}")
