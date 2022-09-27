8200dd3d79fde3053b238e31d1a63f4e21091df0
Ric Evans <emejqz@gmail.com>
2022-09-27 18:33:33 -0500

    move check to earlier

--- resources/local-gcp-emulator.sh
+++ resources/local-gcp-emulator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$PUBSUB_EMULATOR_HOST" -neq "localhost:8085" ]; then
+    echo "Don't forget to 'export PUBSUB_EMULATOR_HOST=localhost:8085'"
+    exit 1
+fi
+
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get install apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
@@ -7,10 +12,5 @@ sudo apt-get update
 sudo apt-get install google-cloud-sdk
 sudo apt-get install google-cloud-sdk-pubsub-emulator
 gcloud components list
-
-if [ "$PUBSUB_EMULATOR_HOST" -neq "localhost:8085" ]; then
-    echo "Don't forget to 'export PUBSUB_EMULATOR_HOST=localhost:8085'"
-    exit 1
-fi
 
 gcloud beta emulators pubsub start --project="i3-gcp-proj" &
