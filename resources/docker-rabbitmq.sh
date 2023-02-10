223cf1b336744005208e936081164620961625ea
Ric Evans <ric@evans-jacquez.com>
2023-02-10 10:59:18 -0600

    add `resources/rabbitmq.config` & mount to docker container

--- resources/docker-rabbitmq.sh
+++ resources/docker-rabbitmq.sh
@@ -7,9 +7,18 @@ DOCKERIZE_VERSION=v0.6.1
 
 wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && sudo tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 
+if [ -z $1 ]; then
+    MOUNTS=""
+else
+    MOUNTS="--mount type=bind,source={$(dirname $(realpath $1))},target=/etc/rabbitmq"
+fi
+
+set -x
 docker run -i -d --rm \
     -p 5672:5672 \
-    deadtrickster/rabbitmq_prometheus:3.7
+    -p 15672:15672 \
+    $MOUNTS \
+    deadtrickster/rabbitmq_prometheus:3.7 \
 dockerize -wait tcp://localhost:5672 -timeout 10m
 
 echo "--------------------------------------------------------------"
