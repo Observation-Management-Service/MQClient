aa3db78575779d5235a7aeefc57061739d2bc517
Ric Evans <ric@evans-jacquez.com>
2023-02-10 15:47:32 -0600

    dump additional broker logs on exit

--- resources/docker-rabbitmq.sh
+++ resources/docker-rabbitmq.sh
@@ -8,18 +8,20 @@ DOCKERIZE_VERSION=v0.6.1
 wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && sudo tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 
 if [ -z $1 ]; then
-    MOUNTS=""
+    CUSTOM_CONF_MOUNT=""
 else
-    MOUNTS="-v $(realpath $1):/bitnami/rabbitmq/conf/custom.conf:ro"
+    CUSTOM_CONF_MOUNT="-v $(realpath $1):/bitnami/rabbitmq/conf/custom.conf:ro"
 fi
 
 set -x
+mkdir ./broker_logs
 docker run -i --rm \
     -p 5672:5672 \
     -p 15672:15672 \
      --env RABBITMQ_USERNAME=guest \
      --env RABBITMQ_PASSWORD=guest \
-    $MOUNTS \
+    $CUSTOM_CONF_MOUNT \
+    --mount type=bind,source=$(realpath ./broker_logs),target=/opt/bitnami/rabbitmq/var/log/rabbitmq/
     bitnami/rabbitmq:latest \
     >> broker.out 2>&1 &
 dockerize -wait tcp://localhost:5672 -timeout 10m
