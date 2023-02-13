08b6bc8d41d40ff5694842859d0c8b2524e2ec5f
David Schultz <david.schultz@icecube.wisc.edu>
2023-02-13 17:12:50 -0600

    use host networking to connect to keycloak properly

--- resources/docker-rabbitmq.sh
+++ resources/docker-rabbitmq.sh
@@ -23,8 +23,7 @@ fi
 set -x
 mkdir ./broker_logs
 docker run -i --rm --name $1 \
-    -p 5672:5672 \
-    -p 15672:15672 \
+     --network=host \
      --env RABBITMQ_USERNAME=guest \
      --env RABBITMQ_PASSWORD=guest \
     $CUSTOM_CONF_MOUNT \
