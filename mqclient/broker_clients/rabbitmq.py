43c4aff92ac246163eede4b69714ccace0a0e2bc
Ric Evans <ric@evans-jacquez.com>
2023-02-13 12:12:08 -0600

    move `basic_qos(prefetch_count=self.prefetch, global_qos=True)` to Sub

--- mqclient/broker_clients/rabbitmq.py
+++ mqclient/broker_clients/rabbitmq.py
@@ -62,7 +62,7 @@ class RabbitMQ(RawQueue):
         LOGGER.info(f"Connecting with parameters={self.parameters}")
         self.connection = pika.BlockingConnection(self.parameters)
         self.channel = self.connection.channel()
-        self.channel.basic_qos(prefetch_count=self.prefetch, global_qos=True)
+        self.channel.basic_qos(global_qos=True)
         """
         We need to discuss how many RabbitMQ instances we want to run
         the default is that the quorum queue is spread across 3 nodes
@@ -176,6 +176,8 @@ class RabbitMQSub(RabbitMQ, Sub):
 
         if not self.channel:
             raise ConnectingFailedException("No channel to configure connection.")
+
+        self.channel.basic_qos(prefetch_count=self.prefetch, global_qos=True)
 
         LOGGER.debug(log_msgs.CONNECTED_SUB)
 
