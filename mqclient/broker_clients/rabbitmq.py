34a3ee92144855a04bce96e89193c6a08a340a21
Ric Evans <ric@evans-jacquez.com>
2023-07-26 15:00:56 -0700

    try `/api/definitions`

--- mqclient/broker_clients/rabbitmq.py
+++ mqclient/broker_clients/rabbitmq.py
@@ -93,7 +93,7 @@ class RabbitMQ(RawQueue):
 
         # TODO: get broker's consumer_timeout & confirm with ack_timeout
         resp = requests.get(
-            f"http://{cp_args['host']}:15672/api/overview",
+            f"http://{cp_args['host']}:15672/api/definitions",
             auth=requests.auth.HTTPBasicAuth(
                 creds.username if creds else "guest",
                 creds.password if creds else "guest",
