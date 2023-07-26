1a9d5f338a4e86f144940474fe23339ced3b3bb1
Ric Evans <ric@evans-jacquez.com>
2023-07-26 16:48:25 -0700

    update tests

--- tests/abstract_broker_client_tests/integrate_broker_client_interface.py
+++ tests/abstract_broker_client_tests/integrate_broker_client_interface.py
@@ -39,10 +39,10 @@ class PubSubBrokerClientInterface:
     async def test_00(self, queue_name: str, auth_token: str) -> None:
         """Sanity test."""
         pub = await self.broker_client.create_pub_queue(
-            "localhost", queue_name, auth_token, None
+            "localhost", queue_name, auth_token
         )
         sub = await self.broker_client.create_sub_queue(
-            "localhost", queue_name, 1, auth_token, None
+            "localhost", queue_name, 1, auth_token
         )
 
         # send
@@ -91,10 +91,10 @@ class PubSubBrokerClientInterface:
         Order is not guaranteed on redelivery.
         """
         pub = await self.broker_client.create_pub_queue(
-            "localhost", queue_name, auth_token, None
+            "localhost", queue_name, auth_token
         )
         sub = await self.broker_client.create_sub_queue(
-            "localhost", queue_name, 1, auth_token, None
+            "localhost", queue_name, 1, auth_token
         )
 
         # send
@@ -162,10 +162,10 @@ class PubSubBrokerClientInterface:
         Order is not guaranteed on redelivery.
         """
         pub = await self.broker_client.create_pub_queue(
-            "localhost", queue_name, auth_token, None
+            "localhost", queue_name, auth_token
         )
         sub = await self.broker_client.create_sub_queue(
-            "localhost", queue_name, 1, auth_token, None
+            "localhost", queue_name, 1, auth_token
         )
 
         data_to_send = copy.deepcopy(DATA_LIST)
@@ -233,10 +233,10 @@ class PubSubBrokerClientInterface:
     async def test_20(self, queue_name: str, auth_token: str) -> None:
         """Sanity test message generator."""
         pub = await self.broker_client.create_pub_queue(
-            "localhost", queue_name, auth_token, None
+            "localhost", queue_name, auth_token
         )
         sub = await self.broker_client.create_sub_queue(
-            "localhost", queue_name, 1, auth_token, None
+            "localhost", queue_name, 1, auth_token
         )
 
         # send
